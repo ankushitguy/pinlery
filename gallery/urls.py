05a60daeec7a855cff845163df6891691901d159
Maria Hl <imgvoid@gmail.com>
2020-11-07 22:11:30 +0300

    edit

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('gallery/<section_slug>/', views.showcase, name='pins_page'),
+    path('gallery/<section_slug>/', views.showcase, name='showcase'),
     path('boards/', views.create_boards, name='create_boards'),
     path('sections/', views.create_sections, name='create_sections'),
     path('pins/', views.create_pins, name='create_pins'),
